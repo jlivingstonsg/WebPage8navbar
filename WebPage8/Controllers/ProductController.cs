0ef4d5d95efdc190fd699d045219ed766c883530
jlivingstonsg <magnus.ivarsson@gmail.com>
2021-10-20 16:13:28 +0200

    Add project files.

---
+++ WebPage8/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebPage8.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult BrandItems()
+        {
+            return View();
+        }
+        public IActionResult Detail()
+        {
+            return View();
+        }
+        public IActionResult Items()
+        {
+            return View();
+        }
+    }
+}
